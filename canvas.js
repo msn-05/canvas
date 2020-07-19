e53445f3e71ed01f76c7070cfd17ca3aa66de726
nuheen123 <68454203+nuheen123@users.noreply.github.com>
2020-07-19 16:59:17 +0600

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,11 +1,7 @@
 class Canvas{
-    constructor(width = window.innerWidth,height = window.innerHeight - 20){
+    constructor(width = window.innerWidth-10,height = window.innerHeight-20){
         this.canvas = document.createElement("canvas");
         this.canvas.style.border = "1px solid black";
-        this.canvas.style.position = "absolute";
-        this.canvas.style.top = "50%";
-        this.canvas.style.left = "50%";
-        this.canvas.style.transform = "translate(-50%,-50%)";
         this.ctx = this.canvas.getContext("2d");
         this.canvas.width = width;
         this.canvas.height = height;
