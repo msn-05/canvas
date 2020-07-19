b8e3a1885391cc438b45044177a4836fe2c78a99
nuheen123 <68454203+nuheen123@users.noreply.github.com>
2020-07-19 16:11:21 +0600

    Update file.js

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 class Canvas{
-    constructor(width = window.innerWidth - 40,height = window.innerHeight - 40){
+    constructor(width = window.innerWidth,height = window.innerHeight){
         this.canvas = document.createElement("canvas");
         this.ctx = this.canvas.getContext("2d");
         this.canvas.width = width;
